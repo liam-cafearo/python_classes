20169b7cfd45b962394c29045d7fb7a04b4a825c
liam-cafearo <lcafearo@gmail.com>
2018-03-28 22:37:23 +0100

    Made kind and call private

--- the_bird_classs.py
+++ the_bird_classs.py
@@ -1,7 +1,12 @@
 class Bird:
     def __init__(self, kind, call):
-        self.kind = kind
-        self.call = call
+        # made kind and call private with _kind and _call
+        self._kind = kind
+        self._call = call
     
     def do_call(self):
         print 'a %s goes %s' % (self.kind, self.call)
+
+
+owl = Bird('Owl', 'Twir Twoo!')
+print owl._kind
